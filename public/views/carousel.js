6ec88ae57d7b774b42767a2adf383d52ace16fba
YOUR NAME <erinpagemd@yahoo.com>
2015-05-24 00:40:36 -0500

    working on layout of portfolio page

--- public/views/carousel.js
+++ public/views/carousel.js
@@ -6,4 +6,44 @@ angular
   $scope.gotoHor = function (){
     $location.hash('hor-div');
   }
+
+  $scope.gotoAppchiever = function (){
+    $location.hash('appchiever')
+  }
+
+  $scope.gotoChismosas = function (){
+    $location.hash('chismosas')
+  }
+
+  $scope.gotoNerd = function (){
+    $location.hash('nerd')
+  }
+
+  $scope.gotoBook = function (){
+    $location.hash('book')
+  }
+
+  $scope.gotoRecycle = function (){
+    $location.hash('recycle')
+  }
+
+  $scope.gotoBattle = function (){
+    $location.hash('battle')
+  }
+
+  $scope.gotoWon = function (){
+    $location.hash('won')
+  }
+
+  $scope.gotoHoney = function (){
+    $location.hash('honey')
+  }
+
+  $scope.gotoStock = function (){
+    $location.hash('stock')
+  }
+
+  $scope.gotoPush = function (){
+    $location.hash('push')
+  }
 })
