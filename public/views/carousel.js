50eecf7cc66787c085bdc4ad4dea505327a0d216
Erin Page <erinpagemd@yahoo.com>
2015-06-26 00:00:36 -0500

    pulling out project data into a model

--- public/views/carousel.js
+++ public/views/carousel.js
@@ -1,49 +1,12 @@
 angular
 .module('ErinApp')
-.controller('CarouselCtrl', function ($scope, $location, $anchorScroll){
-  $scope.hovering = false;
+.controller('CarouselCtrl', function ($scope, CarouselService, $location, $anchorScroll){
 
-  $scope.gotoHor = function (){
-    $location.hash('hor-div');
-  }
+  $scope.projects = CarouselService;
 
-  $scope.gotoAppchiever = function (){
-    $location.hash('appchiever')
-  }
 
-  $scope.gotoChismosas = function (){
-    $location.hash('chismosas')
-  }
+  // $scope.gotoHor = function (){
+  //   $location.hash('hor-div');
+  // }
 
-  $scope.gotoNerd = function (){
-    $location.hash('nerd')
-  }
-
-  $scope.gotoBook = function (){
-    $location.hash('book')
-  }
-
-  $scope.gotoRecycle = function (){
-    $location.hash('recycle')
-  }
-
-  $scope.gotoBattle = function (){
-    $location.hash('battle')
-  }
-
-  $scope.gotoWon = function (){
-    $location.hash('won')
-  }
-
-  $scope.gotoHoney = function (){
-    $location.hash('honey')
-  }
-
-  $scope.gotoStock = function (){
-    $location.hash('stock')
-  }
-
-  $scope.gotoPush = function (){
-    $location.hash('push')
-  }
 })
