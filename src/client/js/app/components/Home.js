885397ba374d6ca24a5281453c56b42dc4dc5777
Erin Page <erin@reax.io>
2016-11-29 23:22:03 -0600

    bring in the Speaking class

--- src/client/js/app/components/Home.js
+++ src/client/js/app/components/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import acss from '../utils/acss';
 import HomeIcon from '../../../img/sprite/home.svg';
+import Speaking from './Speaking';
 
 export default class Home extends React.Component {
   render() {
@@ -15,10 +16,12 @@ export default class Home extends React.Component {
 
     return (
       <div {...attrs.div}>
-        <h1 {...attrs.h1}>Home</h1>
+        <h1 {...attrs.h1}>Erin M Page</h1>
+        <p> Web Application Engineer | Nashville, Tennessee </p>
         <svg>
           <use xlinkHref={HomeIcon} />
         </svg>
+        <Speaking />
       </div>
     );
   }
