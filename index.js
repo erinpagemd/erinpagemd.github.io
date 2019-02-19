dcfebff90ad572a22bb20d9d97a29585a665001f
Erin Page <erin@beachyapp.com>
2019-02-18 19:13:48 -0600

    start new site

--- index.js
+++ index.js
@@ -3,15 +3,16 @@
 //var $ = require('jquery'),
 //    _ = require('lodash')
 
-angular
-.module('ErinApp', ['ui.router'])
-.config(function($stateProvider, $urlRouterProvider){
-  $urlRouterProvider.otherwise('/');
+$(document).ready(initialize);
+function initialize () {
+  $('#submit-form').click(function(event) {
+    console.log(event.type);
+    event.preventDefault();
+    $('#name').val("");
+    $('#email').val("");
+    $('#phone').val("");
+    $('#message').val("");
+  })
 
-  $stateProvider
-    .state('home', {url:'/', templateUrl:'views/title.html'})
-    .state('profile', {url:'/profile', templateUrl:'views/carousel.html', controller:'CarouselCtrl'})
-    .state('selfie', {url:'/selfie', templateUrl:'views/selfie.html'})
-    .state('community', {url:'/community', templateUrl:'views/community.html'})
-    .state('contact', {url:'/contact', templateUrl:'views/contactform.html'})
-});
+}
+
